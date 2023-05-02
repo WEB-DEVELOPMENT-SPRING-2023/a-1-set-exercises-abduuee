c115ef4ad0435f51c6a67c6beea75432ab257e32
Abdulrahman KT <113695490+abduuee@users.noreply.github.com>
2023-05-02 20:13:33 +0400

    Create script.js

---
+++ Chapter-6-Javascript/Exercises/Petrol Calculator/script.js
@@ -0,0 +1,12 @@
+ // Assigning two variables and linking the element with the ids named 'Cost' and 'Litres'
+let cost = document.getElementById("Cost");
+let litres = document.getElementById("Litres");
+
+// Defining a function with the name 'Calculate'
+function Calculate() {
+    // Assigning a variable and multiplying the two values
+    let totalcost = cost.value * litres.value;
+    // Displaying the text in the webpagex
+    document.getElementById("totalcost").innerHTML = `Your total cost is ${totalcost}`;
+    return false;
+}
